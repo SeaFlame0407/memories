08322457f65b1d10e091e452c6fc61df1fa4937a
SeaFlame0407 <acoruslead@gmail.com>
2026-05-13 14:03:20 +0800

    Create album.js

---
+++ js/album.js
@@ -0,0 +1,23 @@
+fetch("../data/albums.json")
+  .then(res => res.json())
+  .then(data => {
+      renderAlbums(data);
+  });
+
+function renderAlbums(albums){
+    const container = document.getElementById("list");
+
+    albums.forEach(a => {
+        container.innerHTML += `
+            <a href="${a.path}" class="album-card">
+                <div class="album-cover">
+                    <img src="${a.cover}" alt="Meme collection">
+                </div>
+                <div class="album-info">
+                    <h3>${a.name}</h3>
+                    <p>${a.memo}</p>
+                </div>
+            </a>
+        `;
+    });
+}
