73c88018015e30a5b5cc96cb0344e6595d1a7959
SeaFlame0407 <acoruslead@gmail.com>
2026-05-13 11:04:02 +0800

    Create layout.js

---
+++ js/layout.js
@@ -0,0 +1,10 @@
+function loadComponent(id, file){
+    fetch(file)
+        .then(res => res.text())
+        .then(html => {
+            document.getElementById(id).innerHTML = html;
+        });
+}
+
+loadComponent("header", "/layout/header.html");
+loadComponent("footer", "/layout/footer.html");
